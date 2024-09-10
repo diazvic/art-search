6319d0be1a08db2211f89bde33aa37ca078d95be
Victoria Diaz <65260980+diazvic@users.noreply.github.com>
2024-09-10 15:10:53 +0200

    Update routes  in app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import { DataProvider } from "./context/DataContext";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Card from "./components/Card";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import CardDetail from "./components/CardDetail";
 const App = () => {
 	return (
-		<DataProvider>
-			<NavBar />
-			<div>
-				<Card />
-			</div>
-			<Footer />
-		</DataProvider>
+		<BrowserRouter>
+			<DataProvider>
+				<NavBar />
+				<Routes>
+					<Route path="/" element={<Card />} />
+					<Route path="/detail" element={<CardDetail />} />
+				</Routes>
+				<Footer />
+			</DataProvider>
+		</BrowserRouter>
 	);
 };
 
