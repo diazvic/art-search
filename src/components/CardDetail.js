8887b15873255d1ae98433f7a7d14c4e9303668b
Victoria Diaz <65260980+diazvic@users.noreply.github.com>
2024-09-11 09:34:05 +0200

    Added new changes to cardDetail with the useLocation hook and new styles

---
+++ src/components/CardDetail.js
@@ -0,0 +1,35 @@
+import { useLocation } from "react-router-dom";
+import "../styles/_CardDetail.scss";
+
+const CardDetail = () => {
+	const location = useLocation();
+
+	const { info } = location.state;
+
+	console.log("Received info in detail page:", info);
+
+	return (
+		<div className="card-detail">
+			<div className="detail-container">
+				<img
+					src={`https://www.artic.edu/iiif/2/${info.image_id}/full/843,/0/default.jpg`}
+					alt={info.title}
+				/>
+				<div className="detail-box">
+					<h3>Year: {info.date_start}</h3>
+					<h3>{info.place_of_origin}</h3>
+					<h3>{info.artist_display}</h3>
+					<div className="box-paragraph">
+						<p>
+							{info.publication_history == null
+								? "This piece doesn't have a publication history"
+								: info.publication_history}
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default CardDetail;
