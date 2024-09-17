549b9fddb3c52b9f3bca6eb7526ebeb1ab72382a
Victoria Diaz <65260980+diazvic@users.noreply.github.com>
2024-09-17 10:25:27 +0200

    Added function to form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,31 @@
+import { DataContext } from "../context/DataContext";
+import { useContext, useState } from "react";
 import "../styles/_Form.scss";
 import { CiSearch } from "react-icons/ci";
+import UnitCards from "./UnitCards";
 const Form = () => {
+	const { fetchSearchData, searchResults } = useContext(DataContext);
+	const [searchTerm, setSearchTerm] = useState("");
+
+	const handleClick = () => {
+		console.log("click");
+		fetchSearchData(searchTerm);
+	};
+	const handleChange = (e) => {
+		console.log(e.target.value);
+		setSearchTerm(e.target.value);
+	};
 	return (
-		<form>
+		<form onSubmit={(e) => e.preventDefault()}>
 			<div>
 				<h2>Search</h2>
 				<CiSearch />
-				<input type="search" placeholder="Enter your search here" />
+				<input
+					type="search"
+					placeholder="Enter your search here"
+					value={searchTerm}
+					onChange={handleChange}
+				/>
 			</div>
 			<div className="flex-form">
 				<label>type</label>
@@ -22,7 +41,21 @@ const Form = () => {
 					<option>Z-A</option>
 				</select>
 			</div>
-			<button className="button-form">Search</button>
+			<button className="button-form" type="submit" onClick={handleClick}>
+				Search
+			</button>
+			{searchResults.length > 0 && (
+				<div>
+					<h3>Search Results</h3>
+					<ul>
+						{searchResults.map((info) => (
+							<div key={info.id}>
+								<UnitCards info={info} />
+							</div>
+						))}
+					</ul>
+				</div>
+			)}
 		</form>
 	);
 };
