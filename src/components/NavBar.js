63c2d62558dd03ee0c36a1e8ff515d02895c69ee
Victoria Diaz <65260980+diazvic@users.noreply.github.com>
2024-09-24 15:52:04 +0200

    Added new styles for responsive mobile design

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,12 +2,18 @@ import "../styles/_NavBar.scss";
 import pictureMuseum from "../assets/pictureMuseum.jpeg";
 const NavBar = () => {
 	return (
-		<nav>
-			<h1>Art Institute of Chicago</h1>
+		<>
+			<nav>
+				<h1>Art Institute of Chicago</h1>
+			</nav>
 			<div className="box-image">
-				<img src={pictureMuseum} alt="photo from museum" />
+				<img
+					src={pictureMuseum}
+					alt="photo from museum"
+					className="image-museum"
+				/>
 			</div>
-		</nav>
+		</>
 	);
 };
 
