45131d734077d95ce365619fcf4902585c6c5db1
Victoria Diaz <65260980+diazvic@users.noreply.github.com>
2024-09-25 11:51:45 +0200

    Updated the index.js file to use the latest React version. Implemented conditional rendering for the pagination component using the useLocation hook.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import { DataProvider } from "./context/DataContext";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Card from "./components/Card";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import CardDetail from "./components/CardDetail";
 import Pagination from "./components/Pagination";
-// import Form from "./components/Form";
 const App = () => {
+	const location = useLocation();
 	return (
-		<BrowserRouter>
-			<DataProvider>
-				<NavBar />
-				<Routes>
-					<Route path="/" element={<Card />} />
-					<Route path="/detail" element={<CardDetail />} />
-					{/* <Route path="/search" element={< />} /> */}
-				</Routes>
-				<Pagination />
-				<Footer />
-			</DataProvider>
-		</BrowserRouter>
+		<DataProvider>
+			<NavBar />
+			<Routes>
+				<Route path="/" element={<Card />} />
+				<Route path="/detail" element={<CardDetail />} />
+			</Routes>
+			{location.pathname !== "/detail" && <Pagination />}
+			<Footer />
+		</DataProvider>
 	);
 };
 
