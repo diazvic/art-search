9c8dc5a9c4f3562f40edde75bc719058e1d4f69f
Victoria Diaz <65260980+diazvic@users.noreply.github.com>
2024-09-30 11:31:31 +0200

    Added conditional rendering for pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -17,25 +17,40 @@ const Pagination = () => {
 		currentSearchPage,
 		totalSearchPages,
 	} = useContext(DataContext);
+	console.log(
+		`page ${currentSearch}, ${currentSearchPage} of ${totalSearchPages}`
+	);
+
+	console.log("page total:", totalSearchPages);
+
 	const isFirstPage = currentSearch ? currentSearchPage === 1 : !prevUrl;
 	const isLastPage = currentSearch
 		? currentSearchPage === totalSearchPages
 		: !nextUrl;
 	return (
-		<div className="container-btn-pagination">
-			<button onClick={arrowFirstPage} disabled={isFirstPage}>
-				<MdKeyboardDoubleArrowLeft className="arrow-pagination" />
-			</button>
-			<button onClick={prevUrl} disabled={isFirstPage}>
-				<MdKeyboardArrowLeft className="arrow-pagination" />
-			</button>
-			<button onClick={nextUrl} disabled={isLastPage}>
-				<MdKeyboardArrowRight className="arrow-pagination" />
-			</button>
-			<button onClick={arrowLastPage} disabled={isLastPage}>
-				<MdKeyboardDoubleArrowRight className="arrow-pagination" />
-			</button>
-		</div>
+		<>
+			<div className="container-btn-pagination pagination-box">
+				{currentSearch && (
+					<p>
+						Page: {currentSearchPage} of {totalSearchPages}
+					</p>
+				)}
+			</div>
+			<div className="container-btn-pagination">
+				<button onClick={arrowFirstPage} disabled={isFirstPage}>
+					<MdKeyboardDoubleArrowLeft className="arrow-pagination" />
+				</button>
+				<button onClick={prevUrl} disabled={isFirstPage}>
+					<MdKeyboardArrowLeft className="arrow-pagination" />
+				</button>
+				<button onClick={nextUrl} disabled={isLastPage}>
+					<MdKeyboardArrowRight className="arrow-pagination" />
+				</button>
+				<button onClick={arrowLastPage} disabled={isLastPage}>
+					<MdKeyboardDoubleArrowRight className="arrow-pagination" />
+				</button>
+			</div>
+		</>
 	);
 };
 
